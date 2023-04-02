df405b67606a0bd000708d79999db3c28775b129
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-04-03 00:28:43 +0300

    added the api of downloading a file

--- backend/controllers/student.controllers.js
+++ backend/controllers/student.controllers.js
@@ -1,5 +1,6 @@
 const File = require("../models/file.model");
 const Enrollment = require("../models/enrollment.model");
+const fs = require("fs");
 
 exports.enroll = async (req, res) => {
   const { userId, classId } = req.body;
@@ -22,3 +23,41 @@ exports.getFiles = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.downloadFile = async (req, res) => {
+  try {
+    const { classId, fileId } = req.params;
+    const file = await File.findById(fileId);
+    if (!file) {
+      return res.status(404).send("File not found");
+    }
+    if (file.classId != classId) {
+      return res.status(401).send("Unauthorized access");
+    }
+    const path = file.path;
+    res.download(path);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+};
+
+// exports.downloadFile = async (req, res) => {
+//   try {
+//     const { classId, fileId } = req.params;
+//     const file = await File.findById(fileId);
+//     if (!file) {
+//       return res.status(404).send("File not found");
+//     }
+//     if (file.classId != classId) {
+//       return res.status(401).send("Unauthorized access");
+//     }
+//     const path = file.path;
+//     const filename = file.name;
+//     res.set('Content-Disposition', `attachment; filename="${filename}"`);
+//     res.download(path);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Server error");
+//   }
+// };
--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
-const { enroll, getFiles } = require("../controllers/student.controllers");
+const {
+  enroll,
+  getFiles,
+  downloadFile,
+} = require("../controllers/student.controllers");
 
 router.post("/enroll", enroll);
 router.get("/class/:classId/getFiles", getFiles);
+router.get("/class/:classId/files/:fileId/download", downloadFile);
 
 module.exports = router;
